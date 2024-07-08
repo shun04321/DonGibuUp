34aed1892e760a6a3bc04d4ad937d22240b9d3c7
developer6git <yook127@naver.com>
2024-07-08 17:47:51 +0900

    DBOX VO 작성

--- DontGibuUp/src/main/java/kr/spring/dbox/vo/DboxVO.java
+++ DontGibuUp/src/main/java/kr/spring/dbox/vo/DboxVO.java
@@ -1,5 +1,7 @@
 package kr.spring.dbox.vo;
 
+import java.sql.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +10,26 @@ import lombok.ToString;
 @Setter
 @ToString
 public class DboxVO {
-	
+	private long dbox_num;
+	private long mem_num;
+	private long dcate_num;
+	private int dbox_team_type;
+	private String dbox_team_name;
+	private String dbox_team_detail;
+	private String dbox_team_photo;
+	private int dbox_business_rnum;
+	private String dbox_title;
+	private String dbox_photo;
+	private String dbox_business_plan;
+	private String dbox_budget_data;
+	private String dbox_bank;
+	private int dbox_account;
+	private String dbox_account_name;
+	private String dbox_content;
+	private String dbox_comment;
+	private long dbox_goal;
+	private String dbox_sdate;
+	private String dbox_edate;
+	private Date dbox_rdate;
+	private int dbox_status;
 }
