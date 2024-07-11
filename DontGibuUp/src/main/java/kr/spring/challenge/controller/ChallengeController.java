add9cd79af1130aa69a0df2d18c53cce7207009e
Yujin1039 <shelly103989@gmail.com>
2024-07-11 14:23:39 +0900

    challengeController 수정

--- DontGibuUp/src/main/java/kr/spring/challenge/controller/ChallengeController.java
+++ DontGibuUp/src/main/java/kr/spring/challenge/controller/ChallengeController.java
@@ -1,6 +1,8 @@
 package kr.spring.challenge.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.spring.challenge.service.ChallengeService;
@@ -42,42 +45,30 @@ public class ChallengeController {
 	public String form() {
 		return "challengeWrite";
 	}
-	//챌린지 개설 폼에서 전송된 데이터 처리
+	//챌린지 개설 폼에서 전송된 데이터 유효성 검사 및 결제창 이동
 	@PostMapping("/challenge/write")
-	public String submit(@Valid ChallengeVO challengeVO,BindingResult result, 
-			HttpServletRequest request, HttpSession session,Model model) throws IllegalStateException, IOException {
-		log.debug("<<챌린지 개설 확인>> : "+challengeVO);
+	public String checkValidation(@Valid ChallengeVO challengeVO,BindingResult result, 
+			HttpServletRequest request, HttpSession session, Model model) throws IllegalStateException, IOException {
+		log.debug("<<챌린지 개설 정보 확인>> : "+challengeVO);
 		
 		//유효성 체크
 		if(result.hasErrors()) {
 			return form();
 		}
-		
-		//데이터 추가
-		//회원번호
-		MemberVO member = (MemberVO) session.getAttribute("user");
-		challengeVO.setMem_num(member.getMem_num());
-		//ip
-		challengeVO.setChal_ip(request.getRemoteAddr());
-		//대표 사진 업로드 및 파일 저장
-		challengeVO.setChal_photo(FileUtil.createFile(request, challengeVO.getUpload()));
-		
-		//챌린지 개설
-		challengeService.insertChallenge(challengeVO);
-		
-		//view에 메시지 추가
-		model.addAttribute("message", "챌린지 개설이 완료되었습니다!");
-		model.addAttribute("url", request.getContextPath()+"/challenge/list");
-		
-		return "common/resultAlert";
+			
+		session.setAttribute("challengeVO", challengeVO);
+						
+		//return "redirect:/challenge/챌린지 결제url";
+		return "challengeWrite";
 	}
 	
     @InitBinder
 	public void initBinder(WebDataBinder binder) {
-		//true이면 요청 파라미터의 값이 null이거나 빈 문자열("")일때 변환 처리를 하지 않고 null 값으로 할당.
-		//false이면 변환 과정에서 에러가 발생하고 에러 코드로 "typeMismatch"가 추가됨
 		binder.registerCustomEditor(Integer.class, new CustomNumberEditor(Integer.class,false));
 	}
+    
+    //챌린지 개설 및 결제 처리하기
+    
 	/*==========================
 	 *  챌린지 목록
 	 *==========================*/
