1c25839619bf6caac31179fbd1ddc437f8f65bd8
yookhalim <yookhalim@localhost>
2024-07-13 23:11:26 +0900

    일단 기부박스 데이터베이스 입력까지 완료..(미해결 : 산출근거 추가, 목표금액, 파일처리, 유효성 검사 추가 등)

--- DontGibuUp/src/main/java/kr/spring/dbox/dao/DboxMapper.java
+++ DontGibuUp/src/main/java/kr/spring/dbox/dao/DboxMapper.java
@@ -2,6 +2,9 @@ package kr.spring.dbox.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.spring.dbox.vo.DboxVO;
+
 @Mapper
 public interface DboxMapper {
+	public void insertDbox(DboxVO dbox);
 }
---
+++ DontGibuUp/src/main/java/kr/spring/dbox/vo/DboxBudgetVO.java
@@ -0,0 +1,13 @@
+package kr.spring.dbox.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DboxBudgetVO {
+	private String dbox_bud_purpose;
+	private long dbox_bud_price;
+}
