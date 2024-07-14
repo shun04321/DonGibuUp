de3e46369e1354000b7e00495efa9315b30bc63b
김한철 <@DESKTOP-J246VOT>
2024-07-14 15:59:43 +0900

    .

--- DontGibuUp/src/main/java/kr/spring/payuid/dao/PayuidMapper.java
+++ DontGibuUp/src/main/java/kr/spring/payuid/dao/PayuidMapper.java
@@ -15,10 +15,27 @@ import kr.spring.payuid.vo.PayuidVO;
 public interface PayuidMapper {
 	// 결제 수단 등록 (빌링키 발급)
 	public void registerPayUId(PayuidVO payuidVO);
+	
 	// 결제 카드 삭제 (빌링키 삭제, 수정불가능 재발급 필요)
 	@Delete("DELETE FROM pay_uid WHERE mem_num=#{mem_num}")
 	public void deletePayUId (long mem_num);
-	//정기 결제를 위한 빌링키 가져오기
+	
+	//등록된 카드 가져오기
 	@Select("SELECT * FROM pay_uid WHERE mem_num=#{mem_num}")
 	public List<PayuidVO> getPayUId(long mem_num);
+	
+	//easypay_method와 mem_num으로 payuid 유무 확인 (카드는 이미 존재하는 카드나, 새카드 등록임)
+	// 없으면 해당하는 플랫폼의 payuid 발급 페이지, 
+	// 새카드 등록 눌렀을때도 토스의 카드 payuid 발급 페이지,   
+	//--> payuid 발급 페이지는 무조건 getPayuid.jsp 하지만 subscriptionVO로 받아온
+	// sub_method와 easypay_method에 따라 pg를 다르게 전달함.
+	
+	
+	// 선행조건 1. -> uuid 생성 메소드를 컨트롤러에 만들고 easypay_method의 payuid 유무, 
+	// 새 카드 등록에 따라 사용. 만약 response가 실패로 돌아오면 삭제.
+	
+	// 선행조건 2. -> 오늘 날짜 구하기 메소드를 컨트롤러에 만들고, sub_ndate는 오늘 날짜로 선택,
+	// 즉 오늘 날짜가 정기기부 날짜임을 폼에서 표시, 추후 mypage 정기기부 목록에서 수정할 수 있게 변경
+	
+	// 정기기부시 결제수단에 계좌이체, 핸드폰도 가능한 것 같으니 확인.
 }
--- DontGibuUp/src/main/java/kr/spring/subscription/controller/SubscriptionController.java
+++ DontGibuUp/src/main/java/kr/spring/subscription/controller/SubscriptionController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.spring.category.service.CategoryService;
 import kr.spring.category.vo.DonationCategoryVO;
@@ -42,18 +43,28 @@ public class SubscriptionController {
 	
 	@PostMapping("/category/registerSubscription")
     public String signup(@Validated(ValidationSequence.class) SubscriptionVO subscriptionVO,
-                         BindingResult result,
-                         Model model,
+						/* BindingResult result, */
+    					 Model model,
                          HttpServletRequest request,
                          HttpSession session) {
-		log.debug("subscriptionVO : "+subscriptionVO);
-        if (result.hasErrors()) {
-        	DonationCategoryVO category = categoryService.selectDonationCategory(subscriptionVO.getDcate_num());
-        	model.addAttribute("category", category); // 필요한 데이터 다시 추가
-            model.addAttribute("subscriptionVO", subscriptionVO); // 폼 데이터를 다시 설정
-            return "categoryDetail"; // 에러가 있는 경우 다시 폼을 보여줌
-        }
-
+		log.debug("정기기부 등록 subscriptionVO : "+subscriptionVO);
+		
+		//조건 체크 자바스크립트로 실행
+		/*
+		 * if (result.hasErrors()) { DonationCategoryVO category =
+		 * categoryService.selectDonationCategory(subscriptionVO.getDcate_num());
+		 * model.addAttribute("category", category); // 필요한 데이터 다시 추가
+		 * model.addAttribute("subscriptionVO", subscriptionVO); // 폼 데이터를 다시 설정 return
+		 * "categoryDetail"; // 에러가 있는 경우 다시 폼을 보여줌 }
+		 */
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		
+		if(payuidService.getPayUId() {
+			//payuid가 없는 결제수단을 선택한 경우
+		}
+		// 이미 payuid가 있는 결제수단을 선택한 경우
+		
+	
         // 프로세스 처리
         return "redirect:/category/success"; // 성공시 리다이렉트
     }
