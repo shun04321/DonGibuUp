091c7d5050300910e5a4d258458e1a4ea099cf8b
hancheol Kim <gkscjf137@naver.com>
2024-07-26 16:59:56 +0900

    주말에 일할 준비~

--- DontGibuUp/src/main/java/kr/spring/refund/service/RefundService.java
+++ DontGibuUp/src/main/java/kr/spring/refund/service/RefundService.java
@@ -1,5 +1,23 @@
 package kr.spring.refund.service;
 
-public interface RefundService {
+import java.util.List;
+import java.util.Map;
 
+import kr.spring.refund.vo.RefundVO;
+
+public interface RefundService {
+	//환불 신청
+		public void insertRefund(RefundVO refundVO);
+		
+		//관리자 환불신청 목록 조회
+		public List<RefundVO> getRefundList(Map<String,Object> map);
+		
+		//사용자 환불신청 목록 조회
+		public List<RefundVO> getRefundListByMemnum(Map<String,Object> map);
+		
+		//환불 취소(삭제)
+		public void deleteRefund(long refund_num);
+		
+		//환불 승인 및 반려
+		public void updateRefundStatus(long refund_num, int status);
 }
