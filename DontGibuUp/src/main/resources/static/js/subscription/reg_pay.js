60261c67bbe076f5cb399022e4bdf5b8df71f7db
김한철 <@DESKTOP-J246VOT>
2024-07-29 00:40:11 +0900

    오늘 날짜가 28일이 넘으면 정기기부 날자가 매월 31일로 지정되던 오류 수정

--- DontGibuUp/src/main/resources/static/js/subscription/reg_pay.js
+++ DontGibuUp/src/main/resources/static/js/subscription/reg_pay.js
@@ -115,9 +115,8 @@ $(document).ready(function () {
         	let nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
         	//만약 오늘 날짜가 28일 이후라면 마지막 날짜가 됨.
         	if (today.getDate() > 28) {
-            nextMonth = new Date(today.getFullYear(), today.getMonth() + 2, 0); // 다음 달의 마지막 날짜 계산
-      		  }
-        	let formattedDate = nextMonth.getDate(); // 날짜만 추출
-            $("#paymentDateInfo").text(formattedDate);
-            $("#sub_date").val(formattedDate.toString());
-		});
+     
+            $("#paymentDateInfo").text(28);
+            $("#sub_date").val(28);
+	}
+});
